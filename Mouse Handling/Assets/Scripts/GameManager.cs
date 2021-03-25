98b4d0527920ed4189263064708c22c2971e8676
vian0nguyen <vian0nguyen@gmail.com>
2021-03-24 21:37:28 -0400

    spawners, new assets, and trial + error o my!
    1) unhooked functionality from the spawn script so its all being controlled by a game manager (POGGERS)
    2) dealt with a lot of public vs private game object stuff until u realize.. u forgot to hook up ur scripts (FACEPALM)
    3) made some placeholder assets for the text messages (its like a funny game about text message bubbles rn lol)
    4) wrassled with coroutines cause my spawners liked to vomit all at once and that was a lot of trial and error logic :sweat_smile:  BUT we got it obvi! ! !

--- Mouse Handling/Assets/Scripts/GameManager.cs
+++ Mouse Handling/Assets/Scripts/GameManager.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Spawn spawner;
-    // Start is called before the first frame update
-    /* void Start()
+    public Spawn sender;
+    public Spawn reciever;
+    public float waitTime;
+
+
+    //TODO: move this code ONTO the messages themselves
+    //start functionality where when a sender message is selected a 
+    //reciever message responds
+    /* private void OnMouseDown() //this doesnt work unless ON the object
      {
-         spawner = GameObject.GetComponent<Spawn>();
+         Debug.Log("press down");
+         sender.spawnNow = true;
+         StartCoroutine("WaitSpawnTimes");
+     }
+
+     private void OnMouseUp()
+     {
+         reciever.spawnNow = true;
+         StartCoroutine("WaitSpawnTimes");
      }*/
+
+    //test function
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            sender.spawnNow = true;
+            StartCoroutine("WaitSpawnTimes");
+
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("release");
+            reciever.spawnNow = true;
+            StartCoroutine("WaitSpawnTimes");
+        }
+
+    }
+
+    IEnumerator WaitSpawnTimes()
+    {
+        yield return new WaitForFixedUpdate();//huh ive never used this but its nice and makes the code go
+        sender.spawnNow = false;
+        reciever.spawnNow = false;
+        yield return new WaitForSeconds(waitTime);
+    }
 
     //ATM not working idk why :shrug:!
     //TODO: input functionality unhinged from Spawn script
     //INK integration https://www.youtube.com/watch?v=YW4YdsBtC38&list=PLlXuD3kyVEr5V8bK9TnEptHgoa_mYMTjb&index=2 
-    private void OnMouseDown()
-    {
-        spawner.spawnNow = true;
-    }
-
-    private void OnMouseUp()
-    {
-        spawner.spawnNow = true;
-    }
+   
 }
--- Mouse Handling/Assets/Scripts/Spawn.cs
+++ Mouse Handling/Assets/Scripts/Spawn.cs
@@ -15,48 +15,21 @@ public class Spawn : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //TODO: detatch input to game manager
-        //only use spawn.cs to house functionality
-        //call funcationality in other scripts.
-        if (Input.GetButtonDown("Jump"))
+
+        if (spawnNow == true)
         {
-            spawnNow = true;
-            if(spawnNow == true)
-        {
-                //StartCoroutine("WaitSpawnTimes");
-
-                Debug.Log("down");
-                // Instantiate the projectile at the position and rotation of this transform
-                GameObject clone;
-                clone = Instantiate(spawnObject, transform.position, transform.rotation);
-
-
-            }
-            StartCoroutine("WaitSpawnTimes");
+            Debug.Log("now spawning");
+            // Instantiate the projectile at the position and rotation of this transform
+            GameObject clone;
+            clone = Instantiate(spawnObject, transform.position, transform.rotation);
         }
-
-        
+   
     }
 
-    /*void SpawnIt()
-    {
-        for (int i = 0; i < 1; i++) //if you dont do this it spawns a billion at once lol
-        {
-            GameObject tmp = Instantiate(spawnObject);
-            
-           // StartCoroutine("WaitSpawnTimes");
-        }
-            
-    }*/
 
 
-    IEnumerator WaitSpawnTimes()
-    {
-        spawnNow = false;
-        yield return new WaitForSeconds(waitTime);
-        
 
-    }
+    
 }
